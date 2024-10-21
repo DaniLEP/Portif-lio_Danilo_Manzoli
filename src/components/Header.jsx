87d1d400b009c64b80e7701ae7d01bcd8d1c52b1
DaniLEP <danilomanzoli64@gmail..com>
2024-10-21 20:36:56 -0300

    fix: ajuste de rotas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ export default function Header() {
 
         {/* Menu Desktop */}
         <ul className="hidden md:flex space-x-4">
-          <li><a href="/about/" className="hover:text-gray-400">Sobre Mim</a></li>
+          <li><a href="/about" className="hover:text-gray-400">Sobre Mim</a></li>
           <li><a href="#awards" className="hover:text-gray-400">Certificações</a></li>
           <li><a href="#projects" className="hover:text-gray-400">Projetos</a></li>
           <li><a href="#contact" className="hover:text-gray-400">Contato</a></li>
@@ -50,7 +50,7 @@ export default function Header() {
         {/* Menu Mobile */}
         {menuOpen && (
           <ul className="absolute top-16 left-0 w-full bg-[#191970] flex flex-col items-center space-y-4 p-4 md:hidden">
-            <li><a href="#about" className="hover:text-gray-400" onClick={toggleMenu}>Sobre Mim</a></li> 
+            <li><a href="/about" className="hover:text-gray-400" onClick={toggleMenu}>Sobre Mim</a></li> 
             <li><a href="#awards" className="hover:text-gray-400" onClick={toggleMenu}>Certificações</a></li><hr />
             <li><a href="#projects" className="hover:text-gray-400" onClick={toggleMenu}>Projetos</a></li><hr />
             <li><a href="#contact" className="hover:text-gray-400" onClick={toggleMenu}>Contato</a></li>
